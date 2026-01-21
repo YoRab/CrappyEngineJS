04b04d7319de171f60c0792ca1d3a3d153524cbc
Yohann Rabatel <yohann.rabatel@gmail.com>
2026-01-21 16:35:12 +0100

    update shapes positions

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -2,7 +2,7 @@ import { createCube } from './utils/shapes/cube'
 import { createSphere } from './utils/shapes/sphere'
 
 const cube = createCube({ x: 0, y: 0, z: 0 })
-const cube2 = createCube({ x: 3, y: 2, z: 1 }, 2)
+const cube2 = createCube({ x: 3, y: 1, z: 4 }, 2)
 const cube3 = createCube({ x: -4, y: -2, z: -1 }, 3)
 
 const sphere = createSphere({ x: 0, y: -5, z: 0 }, 1.5)
